e5fea494c0d63eb4af78ac8c572336af97a20c96
Sourav Kumar Das <sourav.das@ayata.com>
2022-10-16 13:16:38 +0530

    Day-2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,8 +9,13 @@ import ConfirmOrder from "./components/cart/ConfirmOrder";
 import PaymentSuccess from "./components/cart/PaymentSuccess";
 import Login from "./components/login/Login";
 import Profile from "./components/profile/Profile";
-
-
+import MyOrders from "./components/myOrders/MyOrders";
+import OrderDetails from "./components/myOrders/OrderDetails";
+import Dashboard from "./components/admin/Dashboard";
+import Users from "./components/admin/Users";
+import Orders from "./components/admin/Orders";
+import About from "./components/about/About";
+import NotFound from "./components/layout/NotFound";
 
 import "./styles/app.scss";
 import "./styles/header.scss";
@@ -25,8 +30,10 @@ import "./styles/confirmOrder.scss";
 import "./styles/paymentsuccess.scss";
 import "./styles/login.scss";
 import "./styles/profile.scss";
-
-
+import "./styles/table.scss";
+import "./styles/orderDetails.scss";
+import "./styles/dashboard.scss";
+import "./styles/about.scss";
 
 function App() {
   return( 
@@ -37,12 +44,19 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/contact" element={<Contact />} />
+      <Route path="/about" element={<About />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/shipping" element={<Shipping />} />
       <Route path="/confirmorder" element={<ConfirmOrder />} />
       <Route path="/paymentsuccess" element={<PaymentSuccess />} />
       <Route path="/login" element={<Login />} />
       <Route path="/me" element={<Profile />} />
+      <Route path="/myorders" element={<MyOrders />} />
+      <Route path="/order/:id" element={<OrderDetails />} />
+      <Route path="/admin/dashboard" element={<Dashboard />} />
+      <Route path="/admin/users" element={<Users />} />
+      <Route path="/admin/orders" element={<Orders />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
     <Footer />
   </Router>
