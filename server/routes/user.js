c214a4edcb8c4009528bd9c4ec766fddce961052
Sourav Kumar Das <sourav.das@ayata.com>
2022-10-17 13:50:58 +0530

    backend v-1

---
+++ server/routes/user.js
@@ -0,0 +1,29 @@
+import express from "express";
+import passport from "passport";
+
+
+
+const router = express.Router();
+
+router.get(
+    "/googlelogin",
+    passport.authenticate("google", {
+      scope: ["profile"],
+    })
+  );
+
+// router.get(
+//     "/login",
+//     passport.authenticate("google", {
+//         successRedirect: process.env.FRONTEND_URL
+//     })
+// )
+router.get(
+    "/login", (req, res, next) => {
+        res.send("Logged In")
+    }
+)
+
+
+
+export default router;
